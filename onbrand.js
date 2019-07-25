c193903d9adb65910e049bee87ffe89427dc7e47
ShawnParrotte2 <53319377+ShawnParrotte2@users.noreply.github.com>
2019-07-25 15:45:39 -0700

    Added onbrand.js

---
+++ onbrand.js
@@ -0,0 +1,95 @@
+window.onbrandLoaded = false;
+    /**
+     *  Header & Footer Ajax
+     */
+    var subdir = "chorusV2",
+        version = 1;
+
+    /* Insert Header */
+    $.ajax({
+        url: 'https://raw.githubusercontent.com/ShawnParrotte2/uberflip/master/header.html',
+    }).done(function(data) {
+        $('body').prepend(data);
+    });
+
+    /* Insert Footer */
+    $.ajax({
+        url: 'https://raw.githubusercontent.com/ShawnParrotte2/uberflip/master/footer.html',
+    }).done(function(data) {
+        $('body > footer').remove();
+        $('body').append(data);
+    });
+
+    /**
+     *  Custom Functions
+     */
+
+    function headerWhite() {
+        if ($('body').hasClass('hub-page')) {
+            if ($(window).scrollTop() > 0) {
+                $('#header-outer').addClass('small-nav');
+                console.log('scroll worked');
+            } else {
+                $('#header-outer').removeClass('small-nav');
+            }
+        } else {
+            $('#header-outer').addClass('small-nav');
+            console.log('scroll worked');
+        }
+    }
+
+    function stickyNav() {
+        if ($('body').hasClass('hub-page')) {
+            var followAt = $('.large-header').scrollTop() + $('.large-header').height() - 86;
+            if ($(window).scrollTop() >= followAt) {
+                $('.top-nav').addClass('stickyNav');
+            } else {
+                $('.top-nav').removeClass('stickyNav');
+            }
+        } else {
+            $('.top-nav').removeClass('stickyNav');
+        }
+    }
+
+    $(document).on('click', '.slide-out-widget-area-toggle.mobile-icon.fullscreen-alt', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+        $('.customMenu').toggleClass('active');
+
+    });
+
+    /**
+     *  Events
+     */
+
+    Hubs.Events.on('load', function() {
+
+if (!window.onbrandLoaded) {
+        fixShareWidget();
+        headerWhite();
+window.onbrandLoaded = true
+}
+
+    }).on('pageChange', function() {
+
+    }).on('itemsLoaded', function() {
+
+    }).on('resize', function() {
+        sideCtaFix();
+        addThisFix();
+    }).on('scroll', function() {
+        headerWhite();
+        sideCtaFix();
+        addThisFix();
+        stickyNav();
+    }).on('ctaActivate', function() {
+
+    }).on('ctaFormSubmitSuccess', function() {
+
+    }).on('search', function() {
+
+    });
+
+if (!window.onbrandLoaded) {
+	Hubs.Events.trigger('load');
+}
